fa60fa9c5da0df6fe1f32eae4d5131d00f23738e
jeetuw <jeet63240@gmail.com>
2023-08-17 16:41:53 +0530

    Update finel code

--- index.js
+++ index.js
@@ -1,81 +1,126 @@
-require("dotenv").config();
+// set up an express app
+const port = 3000;
+
 const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
 const dns = require("dns");
-const app = express();
-const validator = require("validator");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
 const mongoose = require("mongoose");
-const internal = require("stream");
+
+// set up an express app
+const app = express();
+
+// mount the body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// render the stylesheet as found in the public folder
+app.use(express.static(`${__dirname}/public`));
+
 mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.l3ja2io.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://communitycoder3:hKMASIoylKzHWZOO@cluster0.pn4wuwk.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
 );
 
-const Url = mongoose.model("Url", { original_url: String, short_url: Number });
-
-// Basic Configuration
-const port = process.env.PORT || 3000;
-app.use(express.urlencoded());
-app.use(express.json());
-
-app.use(cors());
-
-app.use("/public", express.static(`${process.cwd()}/public`));
-
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+const { Schema } = mongoose;
+const urlSchema = new Schema({
+  original_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+// define a model, on which all instances (documents) will be based
+const Url = mongoose.model("Url", urlSchema);
+
+// EXPRESS && ROUTING
+// in the root path render the HTML file as found in the views folder
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.post("/api/shorturl", async function (req, res) {
-  let formData = req.body.url;
-  formData = formData.trim();
-
-  let isCorrect = validator.isURL(formData);
-
-  if (!isCorrect) {
-    res.json({ error: "invalid url" });
-  } else {
-    // steps
-    // 1) Store exact data in mongodb
-    // 2) Send saved response
-    formData = formData.toLowerCase();
-    const totalDocsInUrlCollection = await Url.countDocuments();
-    const webUrl = new Url({
-      original_url: formData,
-      short_url: totalDocsInUrlCollection,
-    });
-    const savedData = await webUrl.save();
-
-    res.json({
-      original_url: savedData["original_url"],
-      short_url: savedData["short_url"],
-    });
-  }
-});
-
-app.get("/api/shorturl/:index", async (req, res) => {
+app.post("/api/shorturl", async (req, res) => {
   try {
-    let index = req.params.index;
-    const data = await Url.findOne({ short_url: parseInt(index) });
+    const urlRequest = req.body.url;
 
-    if (data) {
-      res.redirect(data["original_url"]);
+    const hostname = urlRequest
+      .replace(/http[s]?\:\/\//, "")
+      .replace(/\/(.+)?/, "");
+
+    const addresses = await new Promise((resolve, reject) => {
+      dns.lookup(hostname, (lookupErr, addresses) => {
+        if (lookupErr) {
+          reject("lookup() error");
+        }
+        resolve(addresses);
+      });
+    });
+
+    if (!addresses) {
+      res.json({
+        error: "invalid URL",
+      });
     } else {
-      res.json({ error: "No short URL found for the given input" });
+      const urlFound = await Url.findOne({
+        original_url: urlRequest,
+      });
+
+      if (!urlFound) {
+        const count = await Url.estimatedDocumentCount();
+
+        const url = new Url({
+          original_url: urlRequest,
+          short_url: count + 1,
+        });
+
+        const urlSaved = await url.save();
+
+        res.json({
+          original_url: urlSaved.original_url,
+          short_url: urlSaved.short_url,
+        });
+      } else {
+        res.json({
+          original_url: urlFound.original_url,
+          short_url: urlFound.short_url,
+        });
+      }
     }
-  } catch (e) {
-    res.json({ error: "No short URL found for the given input" });
+  } catch (error) {
+    console.error(error);
+    res.send("An error occurred.");
   }
 });
 
-app.get("*", (req, res) => {
-  res.send("Not Found");
+// following a get request in the selected path, re-route the visitor toward the unshortened url
+app.get("/api/shorturl/:shorturl", async (req, res) => {
+  try {
+    const { shorturl } = req.params;
+
+    const urlFound = await Url.findOne({
+      short_url: shorturl,
+    });
+
+    if (!urlFound) {
+      res.json({
+        error: "no matching URL",
+      });
+    } else {
+      res.redirect(urlFound.original_url);
+    }
+  } catch (error) {
+    console.error(error);
+    res.send("An error occurred.");
+  }
 });
 
-app.listen(port, function () {
-  console.log(`Listening on port ${port}`);
-});
+// listen in the selected port and render the simple application
+app.listen(port);
+console.log(`listening on port ${port}`);
