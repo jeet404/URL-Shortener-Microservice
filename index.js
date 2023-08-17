cc9bd8cab6be946f3327ac7af015acf4c91b494e
Jeet Patel <88264658+jeet404@users.noreply.github.com>
2023-08-17 11:47:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-1.function to manage local file storage (File data.json)
+// 1.function to manage local file storage (File data.json)
 function dataManagement(action, input) {
   let filePath = "./public/data.json";
   //check if file exist -> create new file if not exist
